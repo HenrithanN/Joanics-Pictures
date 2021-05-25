163ab741b41d0608526a5205088e153f2fb9c18e
HenrithanN <61565111+HenrithanN@users.noreply.github.com>
2021-05-25 13:20:04 -0300

    Delete shared directory

--- shared/components/loading/loading-type.ts
+++
@@ -1,5 +0,0 @@
-export enum LoadingType {
-
-  LOADING = 'loading',
-  STOPPED = 'stopped'
-}
--- shared/directives/darken-on-hover/darken-on-hover.module.ts
+++
@@ -1,9 +0,0 @@
-import { DarkenOnHoverDirective } from './darken-on-hover.directive';
-import { NgModule } from '@angular/core';
-
-@NgModule({
-  declarations: [DarkenOnHoverDirective],
-  exports: [DarkenOnHoverDirective]
-})
-
-export class DarkenOnHoverModule { }
